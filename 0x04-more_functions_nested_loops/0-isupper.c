2d32010d568839e093fda328b62733760c011e29
Risachub <ruthyisaac059@gmail.com>
2023-04-10 02:43:43 -0700

    0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,7 +3,7 @@
 /**
   * _isupper - check if a letter is upper
   *@c: The number to be checked
-  *Return 1 for upper later or zero for anyone else
+  *Return: 1 for upper later or zero for anyone else
   */
 int _isupper(int c)
 {
